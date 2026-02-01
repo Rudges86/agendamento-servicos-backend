5407554778a3c3a9430fbb63e3c39bb1d82a0b6f
Rudge Santos <rudges86@gmail.com>
2026-02-01 18:10:11 -0300

    Adicionando o projeto

---
+++ src/main/java/com/br/agendamentoServices/AgendamentoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.br.agendamentoServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoServicesApplication.class, args);
+	}
+
+}
